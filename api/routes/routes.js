1668ab9b415979e4ab0eed2f9904d0ab0e526954
Divya Chowbey <divyachowbey@duke.edu>
2018-02-11 01:15:10 -0500

    final endpoints

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,9 +4,12 @@ module.exports = function (app) {
     var LINQ = require("node-linq").LINQ;
     const cheerio = require('cheerio');
     const syncrequest = require('sync-request');
-    app.get('/', (req, res) =>{res.render("index");} );
+    const fs = require('fs');
+    const path = require('path');
+    const currentDir = path.join(__dirname);
+    app.get('/', (req, res) => { res.render("index"); });
 
-    app.get('/getCountryData', (req, res) => {
+    app.get('/refreshCountryData', (req, res) => {
         // 
         var request = require("request");
 
@@ -30,43 +33,64 @@ module.exports = function (app) {
                 .Where(function (country) { return country.country == "United States" })
                 .OrderBy(function (country) { return country.rank })
                 .ToArray();
-            arr = arr.slice(0, 10); //get first 10
+            arr = arr.slice(0, 50); //get first 10
             for (let countryOI of arr) {
-                if(countryOI.uri && countryOI.uri!=""){
-                let URL = "https://www.inhersight.com/company/" + countryOI.uri;
-                var filePart = syncrequest('GET', URL);
-                console.log(URL);
-                // console.log("Data ", filePart.getBody('utf8').toString());
-                try{
-                let fileData = filePart.getBody('utf8').toString();
-                let companyRatingHTML = cheerio.load(fileData);
-                // let indexesToCrawl = ["0","3","6",""];
-                for(let i =0;i<43;i+=3){
-                    let companyRatingHTMLKEY = companyRatingHTML('#ratings .frame-950 .clearfix .col').eq(i)
-                    // .eq(i+1)
-                    .html().toString().trim();//.length;
-                    let companyRatingHTMLVal = companyRatingHTML('#ratings .frame-950 .clearfix .col').eq(i+1)
-                    // .eq(i+1)
-                    .html().toString().trim();//.length;
-                    countryOI[companyRatingHTMLKEY.toLowerCase().replaceAll(" ","_")]=companyRatingHTMLVal;
-                    // console.log(companyRatingHTMLI);
-                }
-                
+                if (countryOI.uri && countryOI.uri != "") {
+                    let URL = "https://www.inhersight.com/company/" + countryOI.uri;
+                    var filePart = syncrequest('GET', URL);
+                    console.log(URL);
+                    // console.log("Data ", filePart.getBody('utf8').toString());
+                    try {
+                        let fileData = filePart.getBody('utf8').toString();
+                        let companyRatingHTML = cheerio.load(fileData);
+                        // let indexesToCrawl = ["0","3","6",""];
+                        for (let i = 0; i < 43; i += 3) {
+                            let companyRatingHTMLKEY = companyRatingHTML('#ratings .frame-950 .clearfix .col').eq(i)
+                                // .eq(i+1)
+                                .html().toString().trim();//.length;
+                            let companyRatingHTMLVal = companyRatingHTML('#ratings .frame-950 .clearfix .col').eq(i + 1)
+                                // .eq(i+1)
+                                .html().toString().trim();//.length;
+                            if (companyRatingHTMLKEY && companyRatingHTMLKEY != "") {
+                                countryOI[companyRatingHTMLKEY.toLowerCase().replaceAll(" ", "_")] = companyRatingHTMLVal;
+                            }
+                            // console.log(companyRatingHTMLI);
+                        }
 
-                } catch (e){
-                    console.error("Err : ",e)
-                    //toLowerCase()
-                    //.replaceAll()
-                }
+                        countryOI.forbes_rank = countryOI.rank;
+                        
+                        
+
+                        fs.writeFile(currentDir + '/../../data/countryData.json', JSON.stringify(arr), (err) => {
+                            if (err) throw err;
+                            console.log('The file has been saved!');
+                        });
+
+
+                    } catch (e) {
+                        console.error("Err : ", e)
+                        //toLowerCase()
+                        //.replaceAll()
+                    }
                 }
             }
             res.send(arr);
         });
 
     });
+
+    app.get('/getCountryData', (req, res) => {
+        try {
+            delete require.cache[require.resolve('../../data/countryData')];
+            let companyData = require('../../data/countryData');
+            res.send(companyData);
+        } catch (e) {
+           res.send({ error: "No data found. Please run update json." });
+        }
+    });
 };
 
-String.prototype.replaceAll = function(search, replacement) {
+String.prototype.replaceAll = function (search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
